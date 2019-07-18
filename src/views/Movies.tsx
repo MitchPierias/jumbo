f64ab23add9a5cd4d4b2f7dc9f197c73a1723328
Mitch Pierias <mitch@pierias.com>
2019-07-19 07:14:00 +1000

    Implemented movie card selection

--- src/views/Movies.tsx
+++ src/views/Movies.tsx
@@ -4,19 +4,27 @@ import { TMDBMovie } from "../services/MovieService";
 // Components
 import { MovieCard } from "./../components/Movie";
 
-export const Movies:React.FC<{movies:TMDBMovie[]}> = (props) => {
+interface MoviesProps {
+    movies:TMDBMovie[]
+    onSelectMovie: (id:number) => void
+}
+
+export const Movies:React.FC<MoviesProps> = (props) => {
 
     if (props.movies.length <= 0)
         return <div className="movie-collection">Oops... we can't find any movies with that name</div>
 
     return (
         <div className="movie-collection">
-            {props.movies && props.movies.map(movie => (
+            {props.movies && props.movies.map((movie, idx) => (
                 <MovieCard
                     key={movie.id}
+                    id={idx}
                     title={movie.title}
                     imageSource={movie.poster_path}
                     sentiment={movie.vote_average/10}
+                    releaseDate={movie.release_date}
+                    onSelect={props.onSelectMovie}
                 />
             ))}
         </div>
